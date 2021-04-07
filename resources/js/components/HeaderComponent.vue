053c4ca0efb32618dc128e09b5df85f252f1f600
Kamil-87 <k.kamil87-87@yandex.ru>
2021-04-07 11:59:05 +0500

    fixed

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <router-link to="/">Главная</router-link>
+                </li>
+                <li>
+                    <router-link to="/about">О нас</router-link>
+                </li>
+                <li>
+                    <router-link to="/admin">Админка</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/MainComponent.vue
+++ resources/js/components/MainComponent.vue
@@ -1,27 +1,21 @@
 <template>
     <div>
-        <h2>Входной компонент. Главный</h2>
-        <ul>
-            <li>
-                <router-link to="/">Главная</router-link>
-            </li>
-            <li>
-                <router-link to="/about">О нас</router-link>
-            </li>
-            <li>
-                <router-link to="/admin">Админка</router-link>
-            </li>
-        </ul>
+        <Header/>
         <div>
             <router-view></router-view>
         </div>
+        <Footer/>
     </div>
 </template>
 
 <script>
-  export default {
-    name: "MainComponent"
-  }
+import HeaderComponent from "./HeaderComponent";
+import FooterComponent from "./FooterComponent";
+
+export default {
+    name: "MainComponent",
+    components: {HeaderComponent, FooterComponent}
+}
 </script>
 
 <style scoped>
