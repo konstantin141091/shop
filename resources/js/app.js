b4048d5ee4b9a31060ee15db96520897dfb6d6f2
Kamil-87 <k.kamil87-87@yandex.ru>
2021-04-26 16:19:48 +0500

    первые шаги по валидации, сделал валидацию в FeedbackComponent.vue/ подключил Vuelidate

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,31 @@
 import axios from 'axios'
 import Vue from 'vue'
+import Vuelidate from "vuelidate/src"
 import VueRouter from 'vue-router'
-import Main from './layout/MainLayout'
 import router from './router'
-import dateFilter from "./utils/date.filter";
-import {store} from './store';
+import {store} from './store'
+import Main from './layout/MainLayout'
+import dateFilter from "./utils/date.filter"
 
 //Global styles
 import '../sass/app.scss'
 
-const APP_URL = process.env.MIX_APP_URL;
-console.log("my env variable:");
-console.log(APP_URL);
+const APP_URL = process.env.MIX_APP_URL
+console.log("my env variable:")
+console.log(APP_URL)
 
 
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = APP_URL;
-Vue.filter('date', dateFilter);
-Vue.use(VueRouter);
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = APP_URL
+Vue.filter('date', dateFilter)
+
+Vue.use(VueRouter)
+Vue.use(Vuelidate)
 
 store.dispatch('auth/me').then(() => {
     new Vue({
         store,
         router,
         render: h => h(Main)
-    }).$mount('#app');
+    }).$mount('#app')
 });
