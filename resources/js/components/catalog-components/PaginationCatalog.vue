b6d6678ad56e33cc3bb1571608f2e0e6e6b2e887
Kamil-87 <k.kamil87-87@yandex.ru>
2021-06-09 15:05:46 +0500

    pagination

--- resources/js/components/catalog-components/PaginationCatalog.vue
+++ resources/js/components/catalog-components/PaginationCatalog.vue
@@ -1,153 +1,150 @@
 <template>
-    <div>
-        <Notification
-            :messages="messages"
-        />
+  <div>
+    <Notification
+      :messages="messageAddProduct"
+    />
 
-        <div class="catalog">
-            <ProductCardComponent
-                v-for="item in paginatedData"
-                :key="item.id"
-                :imageUrl="item.img ? imageUrl + item.img : '/images/no_photo.png'"
-                :product-data="item"
-                @addToCart="addToCart(item)"
-            />
-        </div>
-        <div class="pagination" v-if="catalogData.length > usersPerPage">
-            <button
-                class="pagination__btn"
-                :disabled="pageNumber === 1"
-                @click="prevPage"
-            >Предыдущая</button>
-            <div class="page__box">
-                <div
-                    class="page"
-                    v-for="page in pages"
-                    :key="page"
-                    :class="{'page__selected': page === pageNumber}"
-                    @click="pageClick(page)"
-                >
-                    {{page}}
-                </div>
-            </div>
-            <button
-                class="pagination__btn"
-                @click="nextPage"
-            >Следующая</button>
-        </div>
+    <div class="catalog">
+      <ProductCardComponent
+        v-for="item in paginatedData"
+        :key="item.id"
+        :imageUrl="item.img ? `/images/products/` + item.img : '/images/no_photo.png'"
+        :product-data="item"
+        @addToCart="addToCart(item)"
+      />
     </div>
+
+    <Paginate
+      v-if="catalogData.length > elementsPerPage"
+      :page-count="pageCount"
+      :click-handler="pageChangeHandler"
+      :prev-text="'Предыдущая'"
+      :next-text="'Следующая'"
+      :container-class="'pagination'"
+      :page-link-class="'page'"
+      :prev-link-class="'pagination__btn__prev'"
+      :next-link-class="'pagination__btn__next'"
+      v-model="page"
+    />
+  </div>
 </template>
 
 <script>
 import ProductCardComponent from "../ProductCardComponent";
-import { mapActions } from "vuex/dist/vuex.mjs";
+import {mapActions} from "vuex/dist/vuex.mjs";
 import SuccessMessageComponent from "../SuccessMessageComponent";
 import Notification from "../../ui/Notification";
 
 export default {
-    name: 'PaginationCatalog',
-    components: {Notification, SuccessMessageComponent, ProductCardComponent},
-    props: {
-        catalogData: {
-            type: Array,
-            default: () => {
-                return []
-            }
-        }
-    },
-    data() {
-        return {
-            usersPerPage: 9,
-            pageNumber: 1,
-            messages: []
-        }
-    },
-    computed: {
-        imageUrl() {
-            return `/images/products/`
-        },
-        pages() {
-            return Math.ceil(this.catalogData.length / 9)
-        },
-        paginatedData() {
-            const start = (this.pageNumber - 1) * this.usersPerPage
-            const end = start + this.usersPerPage
-            return this.catalogData.slice(start, end);
-        }
-    },
-    methods: {
-        ...mapActions([
-            'ADD_TO_CART'
-        ]),
-        addToCart(data) {
-            this.ADD_TO_CART(data)
-            .then(() => {
-                let timeStamp = Date.now().toLocaleString()
-                this.messages.unshift(
-                    {name: 'Товар добавлен в корзину!', id: timeStamp}
-                )
-            })
-        },
-        pageClick(page) {
-            this.pageNumber = page
-        },
-        prevPage() {
-           if (this.pageNumber > 1) {
-               this.pageNumber--
-           }
-        },
-        nextPage() {
-            if (this.pageNumber <= this.pages - 1) {
-                this.pageNumber++
-            }
-
-        }
+  name: 'PaginationCatalog',
+  components: {Notification, SuccessMessageComponent, ProductCardComponent},
+  props: {
+    catalogData: {
+      type: Array,
+      default: () => {
+        return []
+      }
     }
-
+  },
+  data() {
+    return {
+      elementsPerPage: 9,
+      items: [],
+      messageAddProduct: [],
+      page: 1
+    }
+  },
+  computed: {
+    pageCount() {
+      return Math.ceil(this.catalogData.length / this.elementsPerPage)
+    },
+    paginatedData() {
+      const start = (this.page - 1) * this.elementsPerPage
+      const end = start + this.elementsPerPage
+      return this.catalogData.slice(start, end)
+    }
+  },
+  methods: {
+    ...mapActions([
+      'ADD_TO_CART'
+    ]),
+    pageChangeHandler(page) {
+      this.page = page
+    },
+    addToCart(data) {
+      this.ADD_TO_CART(data)
+      let timeStamp = Date.now().toLocaleString()
+      this.messageAddProduct.unshift(
+        {name: 'Товар добавлен в корзину!', id: timeStamp}
+      )
+    },
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "resources/sass/variables";
+
 .catalog {
-    display: flex;
-    flex-wrap: wrap;
-    column-gap: 3rem;
-    row-gap: 3rem;
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 3rem;
+  row-gap: 3rem;
 }
+
 .pagination {
-    display: flex;
-    align-items: center;
-    margin-top: 3rem;
+  display: flex;
+  align-items: center;
+  margin-top: 3rem;
 
-    .page {
-        width: 3rem;
-        height: 3rem;
-        line-height: 3rem;
-        text-align: center;
-        color: $colorText;
-        border-radius: 5px;
+  .page {
+    display: block;
+    width: 3rem;
+    height: 3rem;
+    line-height: 3rem;
+    text-align: center;
+    color: $colorText;
+    border-radius: 5px;
 
-
-        &:hover {
-            background-color: lighten($colorBtn, 10%);
-            cursor: pointer;
-            color: #ffffff;
-        }
-
-        &__box {
-            margin-left: 3rem;
-            margin-right: 3rem;
-            display: flex;
-            align-items: center;
-        }
+    &:hover {
+      background-color: lighten($colorBtn, 10%);
+      cursor: pointer;
+      color: #ffffff;
     }
 
-    .page + .page {
-        margin-left: 5px;
+    &__box {
+      margin-left: 3rem;
+      margin-right: 3rem;
+      display: flex;
+      align-items: center;
     }
+  }
+
+  .active {
+    a {
+      background-color: $colorBtn;
+      color: #ffffff !important;
+    }
+  }
+
+  .disabled {
+    & > .pagination__btn {
+      &__prev, &__next {
+        color: $grey !important;
+        border-color: transparent;
+        background: $greyLight;
+        cursor: context-menu;
+      }
+    }
+  }
+
+  .page + .page {
+    margin-left: 5px;
+  }
 }
+
 .pagination__btn {
+  &__prev, &__next {
     background: $greyLight;
     color: $colorBtn;
     padding: 12px;
@@ -157,17 +154,21 @@ export default {
     font-weight: bold;
 
     &:hover {
-        border: 1px solid $colorBtn;
+      border: 1px solid $colorBtn;
     }
+
     &:active {
-        background: lighten($greyLight, 10%);
+      background: lighten($greyLight, 10%);
     }
-}
+  }
 
-.page__selected {
-    background-color: $colorBtn;
-    color: #ffffff !important;
-}
+  &__prev {
+    margin-right: 3rem;
+  }
 
+  &__next {
+    margin-left: 3rem;
+  }
+}
 
 </style>
