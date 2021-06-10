6e2f8265abe125c3ffdf0f8eb01ff3ab919d8761
Kamil-87 <k.kamil87-87@yandex.ru>
2021-06-10 16:47:51 +0500

    базовая верстка под mobile/ Home page

--- resources/js/components/home-components/NewArrivalComponent.vue
+++ resources/js/components/home-components/NewArrivalComponent.vue
@@ -46,6 +46,32 @@ export default {
         arrows: false,
         slidesToShow: 4,
         infinite: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2,
+              swipeToSlide: true
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              swipeToSlide: true
+            }
+          }
+        ]
       },
       isDisabled: false,
       loading: true,
